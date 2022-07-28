811f43fa3a3a974a4dc5f5d333a2467a98a25bb4
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2022-07-28 20:33:27 +0300

    New mock images

--- frontend/pages/builds.tsx
+++ frontend/pages/builds.tsx
@@ -59,7 +59,7 @@ const Builds = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <TitleBar active="builds" />
-      <Banner url="/mockImages/landscape.jpg">
+      <Banner url="/mockImages/potatoes-2.png">
         <BannerTitle>Builds</BannerTitle>
         <BannerSubtitle>Browse popular builds</BannerSubtitle>
       </Banner>
